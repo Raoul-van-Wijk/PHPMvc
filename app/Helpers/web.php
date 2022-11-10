19eaaaaf293cf86a9ad4d53fdd7f15f71fa91cac
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-10 13:41:23 +0100

    removed testing code

--- app/Helpers/web.php
+++ app/Helpers/web.php
@@ -1,9 +1,7 @@
 <?php
 use App\Libraries\Route;
 
+// Dont forget to include the route class
+
+
 // This file is used to register all the routes for the application.
-
-
-
-Route::post('home/:id', [HomeController::class, 'index'], 'Posthome');
-Route::get('home/:id', [HomeController::class, 'loadView'], 'home');
