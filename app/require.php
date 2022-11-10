8b431fb86e48a93adbbb4f602598773972566010
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-10 13:43:30 +0100

    made seperate autoload file

--- app/require.php
+++ app/require.php
@@ -3,23 +3,9 @@ use App\Libraries\Route;
 use App\Libraries\Core;
 
 
+require_once 'Config/Config.php';
 
-spl_autoload_register(function($className) {
-    $className = str_replace('\\', '/', $className);
-    $className = explode('/', $className);
-    $className = end($className);
-    if(str_ends_with($className, 'Controller')) {
-        require_once 'Controllers/' . $className . '.php';
-    } else if(str_ends_with($className, 'Model')) {
-        require_once 'Models/' . $className . '.php';
-    } else if(str_ends_with($className, 'Entity')) {
-        require_once 'Entities/' . $className . '.php';
-    } else if (str_ends_with($className, 'Helper')) {
-        require_once 'Helpers/' . $className . '.php';
-    } else {
-        require_once 'Libraries/' . $className . '.php';
-    }
-});
+require_once __DIR__ . './autoload.php';
 
 Route::init();
 require_once 'Helpers/web.php';
@@ -27,11 +13,3 @@ require_once 'Helpers/functions.php';
 // Instantiate core class
 $init = new Core();
 
-require_once 'Config/Config.php';
-
-
-
-
-
-
-?>
