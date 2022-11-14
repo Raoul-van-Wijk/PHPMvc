0c8f30a7b41bddc3d0ca383d62e86f21e7390b06
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-14 16:14:14 +0100

    Added some more functions to model
    added some functions so that the user has more options and saver options to prevent sql injection

--- app/Libraries/Model.php
+++ app/Libraries/Model.php
@@ -10,7 +10,11 @@ class Model
 
   protected string $primaryKey = 'id';
 
+  protected array $fillable = [];
+
   protected string $query;
+
+  protected array $binds = [];
   
   public function __construct(string $dbName = null)
   {
@@ -34,28 +38,6 @@ class Model
     return $this->db->single();
   }
 
-  public function search(array $wheres = [], array | string $orderBy = [], string $order, int $limit = 0) {
-    $query = 'SELECT * FROM ' . $this->databaseTable . ' WHERE ';
-    foreach ($wheres as $where) {
-      $query .= $where . ' AND ';
-    }
-    $query = substr($query, 0, -4);
-
-    if(isset($orderBy)) {
-        $query .= 'ORDER BY' . $orderBy;
-    }
-
-    if(isset($order)) {
-      $query .= ' ' . $order;
-    }
-
-    if(isset($limit)) {
-      $query .= ' LIMIT ' . $limit;
-    }
-    return $this->db->resultSet();
-
-  }
-
   public function select(...$columns)
   {
     $this->query = 'SELECT ';
@@ -71,6 +53,12 @@ class Model
     return $this;
   }
 
+  public function join(string $table, string $column1, string $column2, string $joinType = 'INNER')
+  {
+    $this->query .= ' ' . $joinType . ' JOIN ' . $table . ' ON ' . $column1 . ' = ' . $column2;
+    return $this;
+  }
+
   public function where(...$wheres)
   {
     $this->query .= ' WHERE ';
@@ -81,9 +69,80 @@ class Model
     return $this;
   }
 
+  public function orWhere(...$wheres)
+  {
+    $this->query .= ' OR ';
+    foreach ($wheres as $where) {
+      $this->query .= $where . ' OR ';
+    }
+    $this->query = substr($this->query, 0, -3);
+    return $this;
+  }
+
+  public function whereRaw(string $where, array $binds = [])
+  {
+    $this->query .= ' WHERE ' . $where;
+    $this->binds = $binds;
+    return $this;
+  }
+
+  public function orWhereRaw(string $where, array $binds = [])
+  {
+    $this->query .= ' OR ' . $where;
+    $this->binds = $binds;
+    return $this;
+  }
+
+  public function having(...$havings)
+  {
+    $this->query .= ' HAVING ';
+    foreach ($havings as $having) {
+      $this->query .= $having . ' AND ';
+    }
+    $this->query = substr($this->query, 0, -4);
+    return $this;
+  }
+
+  public function orHaving(...$havings)
+  {
+    $this->query .= ' OR ';
+    foreach ($havings as $having) {
+      $this->query .= $having . ' OR ';
+    }
+    $this->query = substr($this->query, 0, -3);
+    return $this;
+  }
+
+  public function havingRaw(string $having, array $binds = [])
+  {
+    $this->query .= ' HAVING ' . $having;
+    $this->binds = $binds;
+    return $this;
+  }
+
+
+  public function orHavingRaw(string $having, array $binds = [])
+  {
+    $this->query .= ' OR ' . $having;
+    $this->binds = $binds;
+    return $this;
+  }
+
+
+  // Safe to use with user input
   public function orderBy($orderBy, $order)
   {
     $this->query .= ' ORDER BY ' . $orderBy . ' ' . $order;
+    return $this;
+  }
+
+  public function groupBy(...$columns)
+  {
+    $this->query .= ' GROUP BY ';
+    foreach ($columns as $column) {
+      $this->query .= $column . ', ';
+    }
+    $this->query = substr($this->query, 0, -2);
     return $this;
   }
 
@@ -96,9 +155,51 @@ class Model
 
   public function get()
   {
-    $this->db->query($this->query);
-    return $this->db->resultSet();
+    $this->db->query($this->query, $this->binds);
+    if(!empty($this->binds)) {
+      foreach ($this->binds as $key => $value) {
+        $this->db->bind($key, $value);
+      }
+    } else {
+      return $this->db->resultSet();
+    }
   }
 
+  public function create(array $data)
+  {
+    foreach ($data as $key => $value) {
+      if(!in_array($key, $this->fillable)) {
+        die("This column name is not mass assignable or does not exist");
+      }
+    }
+    $this->db->query('INSERT INTO ' . $this->databaseTable . ' (' . implode(', ', array_keys($data)) . ') VALUES (:' . implode(', :', array_keys($data)) . ')');
+    foreach ($data as $key => $value) {
+      $this->db->bind(':' . $key, $value);
+    }
+    return $this->db->execute();
+  }
+
+  public function update(array $data, $id)
+  {
+    $placeholders = '';
+    foreach ($data as $key => $value) {
+      if(!in_array($key, $this->fillable)) {
+        die("This column name is not mass assignable or does not exist");
+      }
+      $placeholders .= $key . ' = :' . $key . ', ';
+    }
+    $this->db->query('UPDATE ' . $this->databaseTable . ' SET '. $placeholders .'  WHERE ' . $this->primaryKey . ' = ' . $id);
+    foreach ($data as $key => $value) {
+      $this->db->bind(':' . $key, $value);
+    }
+    return $this->db->execute();
+  }
   
+
+  public function delete(int $id)
+  {
+    $this->db->query('DELETE FROM ' . $this->databaseTable . ' WHERE ' . $this->primaryKey . ' = :id');
+    $this->db->bind(':id', $id);
+    return $this->db->execute();
+  }
 }
