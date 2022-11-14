a25e8f0f12fff126c118b82d97a152b7fa7e0570
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-11-14 16:15:32 +0100

    Added some functions for routing in controllers

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Controllers;
+
+use App\Libraries\Route;
     class Controller 
     {
+        protected string $nextUrl;
+
         protected function view($view, $data = []) 
         {
             if (file_exists('../app/Views/' . $view . '.php')) 
@@ -16,6 +20,26 @@ namespace App\Controllers;
             {
                 die("View does not exists.");
             }
+        }
+
+
+        public function redirect($route)
+        {
+            $this->nextUrl = Route::getRoute($route);
+            return $this;
+        }
+
+        public function back()
+        {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        }
+
+        public function route($routePath = null, $params = [])
+        {
+            if($routePath) {
+                $this->nextUrl = $routePath;
+            }
+            return header('Location: ' . $this->nextUrl);
         }
 
     }
